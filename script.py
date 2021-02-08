32869407161fe0720e9df5379fb9980b8a08ab25
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-08 10:04:40 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,23 +1,34 @@
+"""
+4) Index and Slicing
 
 """
-16) if / else
-"""
 
-# write an if / else if the following numbers are less than 10
-# uncomment one at a time to test your condition
+word = "CatMat"
 
-x = 11
-##x = 12
-##x = 8
+first =     # assign the first letter of word with indexing
+third =     # assign the third letter of word with indexing
+f_3 =       # assign the first three letters word with slicing
 
-if:  # finish by writing condition here
-    print(f"Yes {x} is less than 10")
-else:
-    print(f"no {x} is not less than 10")
+
+
+
+
 
 
 def main():
-    pass
+    if first == "C":
+        print(f"first: passed test, got {first}, expected C")
+    else:
+        print(f"first: failed test, got {first}, expected C")
+    if third == "t":
+        print(f"third: passed test, got {third}, expected t")
+    else:
+        print(f"third: failed test, got {third}, expected t")
+    if f_3 == "Cat":
+        print(f"f_3: passed test, got {f_3}, expected Cat")
+    else:
+        print(f"f_3: failed test, got {f_3}, expected Cat")
+
 
 
 if __name__ == "__main__":
